5da90b3f47db0cb2dbb5d0406d3dcca26d2d5963
shidg <shjiayeah@gmail.com>
2015-07-14 21:22:24 +0800

    modified:   hadoop+hbase/install.txt 	modified:   shell/autodeploy.sh

--- shell/autodeploy.sh
+++ shell/autodeploy.sh
@@ -1,143 +1,157 @@
 #!/bin/bash
 #autodeploy.sh
 #Run at 20:00 everyday,update source code from svn.and rebuild *.war
-#start a new tomcat to use new *.war
+#push *war to remote server,default test_server
 #Modified by shidg,20150610
 
 
-# Define Variables
-BUILDDIR=/root/trunk
-BUILDDIRB=/root/trunkB
-TA=/usr/local/tomcatA
-TB=/usr/local/tomcatB
-MAVEN_HOME=/usr/local/apache-maven
-SUBVERSIOIN_HOME=/usr/local/subversion
-NGINX_HOME=/Data/app/nginx
-NGINX=${NGINX_HOME}/sbin/nginx
-PROXY_CONF=${NGINX_HOME}/conf/vhosts/feezu.cn
-PATH=$PATH:${MAVEN_HOME}/bin:${SUBVERSION_HOME}/bin:${NGINX_HOME}/sbin
-SVN_SERVER=xx.xx.xx.xx
-REPOSITORY_NAME="wzc/manage/source/trunk"
-SVN_USER=jira
-SVN_PASS=jira
-MAIL_LIST=xxx@qq.com,xxx@xx.cn,xxx@xx.com
-LOG_PATH=/Data/logs/xxx
-
-PROJS2=(manage-web consumer-app manage-metadata manage-datawarehouse manage-report manage-orders)
-PROJCPMTEXT=(metadata report orders analysis manage app)
-
 #Trap Err Function
 function ERRTRAP(){
-    echo "[LINE:$1] Error: Command or function exited with status $?" | mail -s "1.61 deploy faild" xxx@qq.com
+    echo "[LINE:$1] Error: Command or function exited with status $?" | mail -s "test_server redeploy faild" 94586572@qq.com
     exit
 }
 
 trap 'ERRTRAP $LINENO' ERR
 
+# Define Variables
+source ~/.bashrc
+SOURCEDIR=/Data/source/trunk/
+BUILDDIR=/Data/war/trunk
+SVN_SERVER=svn.feezu.cn
+REPOSITORY_NAME="repos/wzc/manage/source/trunk"
+SVN_USER=jira
+SVN_PASS=jira
+SYNC_USER=rsync_user
+TEST=10.10.8.32
+DEV=10.10.8.31
+FINAL_WEB=10.10.8.34
+FINAL_SERVICE=10.10.8.35
+REMOTE_SERVER=$TEST
+MODULE=webapps
+MAIL_LIST=94586572@qq.com,op@feezu.cn,ceshi@feezu.cn,jishu@feezu.cn
+#MAIL_LIST=94586572@qq.com
+
+
+#activemq server addr
+if [ "$1" = "final" ];then
+    REMOTE_SERVER=(${FINAL_WEB} ${FINAL_SERVICE})
+    DIR=(manage-datawarehouse/ manage-metadata/ manage-orders/)
+    POSITION=src/main/resources/
+    FILENAME=msgConfig.properties
+    for D in ${DIR[*]}
+        do  
+            sed -i "s/127.0.0.1/${FINAL_SERVICE}/" $SOURCEDIR/$D$POSITION$FILENAME
+        done
+elif [ "$1" = "dev" ];then
+    REMOTE_SERVER=$DEV
+fi
+
+# begin
+PROJS2=(manage-web consumer-app manage-metadata manage-datawarehouse manage-report manage-orders)
+PROJCPMTEXT=(metadata report orders analysis manage app)
+
+
 if [ "`ls -A ${BUILDDIR}`" = " " ];then
-    svn co svn://${SVN_SERVER}/${REPOSITORY_NAME} ${BUILDDIR} --username ${SVN_USER} --password ${SVN_PASS}
-elif [ "`ls -A ${BUILDDIRB}`" = " " ];then
-    svn co svn://${SVN_SERVER}/${REPOSITORY_NAME} ${BUILDDIRB} --username ${SVN_USER} --password ${SVN_PASS}
+    svn co svn://${SVN_SERVER}/${REPOSITORY_NAME} ${BUILDDIR}/ --username ${SVN_USER} --password ${SVN_PASS}
 fi
 
-OLD_TPORT=`mktemp /tmp/tmpfile1.XXXX`
-if ss -tnl | grep 8080;then
-    echo 8080 > ${OLD_TPORT}
-    START_TIME=`date "+%Y%m%d-%T"`
-#update source code
-    svn up ${BUILDDIRB}| tee /tmp/svninfo
-    VERSION=`sed -n '/revision/p' /tmp/svninfo`
-
+START_TIME=`date "+%Y%m%d-%T"`
+# update source code
+svn up ${SOURCEDIR}
+svn info ${SOURCEDIR} | tee /tmp/svninfo
+sed -i '=' /tmp/svninfo && sed -i 'N;s/\n/-/' /tmp/svninfo
+VERSION=`sed -n '/^[369]/p' /tmp/svninfo | awk -F '-' '{print $2}'`
 #build war packages
-    cd ${BUILDDIRB}/wzc
-    mvn clean package
-    $TA/bin/shutdown.sh
-        if ls $TB/webspps | grep -P ".*\.war";then
-            rm -f $TB/webapps/*war
-        fi
-    for PROJ in ${PROJS2[*]}
-        do
-	        cp ${BUILDDIRB}/${PROJ}/target/*war /Data/war/B/${PROJ}.war-${VERSION}
-	        mv ${BUILDDIRB}/${PROJ}/target/*war $TB/webapps/
-        done
-    for CONTEXT in ${PROJCPMTEXT[*]}
-        do
-	        if [ -d "$TB/webapps/${CONTEXT}" ];then	
-		        rm -rf $TB/webapps/${CONTEXT}
-	        fi
-        done
-#open new tomcat
-    $TB/bin/startup.sh
+cd ${SOURCEDIR}/wzc
+mvn clean package
 
-#proxy to new tomcat
-    sed -i 's/tomcatA/tomcatB/' ${PROXY_CONF}
-    ${NGINX} -s reload
-    END_TIME=`date "+%Y%m%d-%T"`
+# delete old wars
+rm -rf ${BUILDDIR}/*
+
+# move war to /Data/war/trunk
+for PROJ in ${PROJS2[*]}
+    do
+	    mv ${SOURCEDIR}/${PROJ}/target/*war ${BUILDDIR}
+    done
+
+# rsync war to remote server
+if [ "$1" = "final" ];then
+    rsync -az --delete --exclude="app-download" --password-file=/etc/rsync.pass ${BUILDDIR}/{app.war,manage.war} ${SYNC_USER}@${REMOTE_SERVER[0]}::$MODULE
+    rsync -az --delete --exclude="app-download" --password-file=/etc/rsync.pass ${BUILDDIR}/{analysis.war,metadata.war,orders.war,report.war} ${SYNC_USER}@${REMOTE_SERVER[1]}::$MODULE
+
+#restart tomcat on ${REMOTE_SERVER}
+(ssh F_MANAGE "/bin/bash /Data/scripts/restart_tomcat.sh")                                                     
+wait 
+(ssh F_SERVICE "/bin/bash /Data/scripts/restart_tomcat.sh")
+wait
+
+END_TIME=`date "+%Y%m%d-%T"`
+
 cat > /tmp/upinfo <<EOF
 =========================
-server: xx.xx.xx.xx
-source code now: $VERSION
+server:final_server
 Start at:${START_TIME}
 Finish at:${END_TIME}
+Description:
+$VERSION
 EOF
-    mail -s "redeploy success" ${MAIL_LIST} < /tmp/upinfo
-    cat /tmp/upinfo >> ${LOG_PATH}/autodeploy.log
-else
-    echo 8081 > ${OLD_TPORT}
-    START_TIME=`date "+%Y%m%d-%T"`
-#update source code
-    svn up ${BUILDDIR}| tee /tmp/svninfo
-    VERSION=`sed -n '/revision/p' /tmp/svninfo`
-    cd ${BUILDDIR}/wzc
-#build war packages
-    mvn clean package
-    $TB/bin/shutdown.sh
-    if ls $TA/webapps | grep -P ".*\.war";then
-        rm -f $TA/webapps/*war
-    fi
-    for PROJ in ${PROJS2[*]}
-        do
-	        cp ${BUILDDIR}/${PROJ}/target/*war /Data/war/A/${PROJ}.war-${VERSION}
-	        mv ${BUILDDIR}/${PROJ}/target/*war $TA/webapps
-        done
-    for CONTEXT in ${PROJCPMTEXT[*]}
-        do
-	        if [ -d "$TA/webapps/${CONTEXT}" ];then	
-		        rm -rf $TA/webapps/${CONTEXT}
-	        fi
-        done
-#open new tomcat
-    $TA/bin/startup.sh
-    sed -i 's/tomcatB/tomcatA/' ${PROXY_CONF}
-    $NGINX -s reload
-    END_TIME=`date "+%Y%m%d-%T"`
+
+#NOTICE
+mail -s "redeploy success" ${MAIL_LIST} < /tmp/upinfo
+
+#log
+cat /tmp/upinfo >> /Data/logs/wzc/autodeploy.log
+
+#recovery activemq addr
+for D in ${DIR[*]}
+    do  
+        sed -i "s/${FINAL_SERVICE}/127.0.0.1/" $SOURCEDIR/$D$POSITION$FILENAME
+    done
+
+elif [ "$1" = "dev" ];then
+    rsync -az --delete --exclude="app-download" --password-file=/etc/rsync.pass ${BUILDDIR}/ ${SYNC_USER}@${REMOTE_SERVER}::$MODULE
+#restart tomcat on $(REMOTE_SERVER)
+ssh  -n DEV  "/Data/scripts/restart_tomcat.sh"
+
+END_TIME=`date "+%Y%m%d-%T"`
+
 cat > /tmp/upinfo <<EOF
 =========================
-server: XX.XX.XX.XX.
-source code now: $VERSION
+server:dev_server
 Start at:${START_TIME}
 Finish at:${END_TIME}
+Description:
+$VERSION
 EOF
-    mail -s "redeploy success" ${MAIL_LIST} < /tmp/upinfo
-    cat /tmp/upinfo >> ${LOG_PATH}/autodeploy.log
+
+#notice
+mail -s "redeploy success" ${MAIL_LIST} < /tmp/upinfo
+
+#log
+cat /tmp/upinfo >> /Data/logs/wzc/autodeploy.log
+
+else    
+    rsync -az --delete --exclude="app-download" --password-file=/etc/rsync.pass ${BUILDDIR}/ ${SYNC_USER}@${REMOTE_SERVER}::$MODULE
+#restart tomcat on $(REMOTE_SERVER)
+    ssh TEST "/Data/scripts/restart_tomcat.sh" 
+
+END_TIME=`date "+%Y%m%d-%T"`
+
+cat > /tmp/upinfo <<EOF
+=========================
+server:test_server
+Start at:${START_TIME}
+Finish at:${END_TIME}
+Description:
+$VERSION
+EOF
+
+#notice
+mail -s "redeploy success" ${MAIL_LIST} < /tmp/upinfo
+
+#log
+cat /tmp/upinfo >> /Data/logs/wzc/autodeploy.log
 fi
 
-#close old tomcat
-TMPFILE=`mktemp /tmp/tmpfile2.XXXX`
-if grep 8080 ${OLD_TPORT};then
-    ps -ef | grep tomcatA | grep -v "grep"  | awk '{print $2}'> $TMPFILE
-    for TPID in `cat $TMPFILE`
-        do
-            kill -9 $TPID
-        done
-else
-    ps -ef | grep tomcatB | grep -v "grep"  | awk '{print $2}'> $TMPFILE
-    for TPID in `cat $TMPFILE`
-        do
-            kill -9 $TPID
-        done
-fi
-
-#delete tmp file
-rm -f /tmp/{svninfo,upinfo}
-rm -f $OLD_TPORT $TMPFILE
+# Done
+exit 0
