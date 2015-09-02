2fce849b1e775770d91af1208493498e5c551e1a
shidg <shjiayeah@gmail.com>
2015-09-02 10:39:29 +0800

    modified:   shell/autodeploy.sh

--- shell/autodeploy.sh
+++ shell/autodeploy.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
-#autodeploy.sh
-#Run at 20:00 everyday,update source code from svn.and rebuild *.war
-#push *war to remote server,default test_server
-#Modified by shidg,20150610
-
-
-#Trap Err Function
-function ERRTRAP(){
-    echo "[LINE:$1] Error: Command or function exited with status $?" | mail -s "test_server redeploy faild" 94586572@qq.com
-    exit
-}
-
-trap 'ERRTRAP $LINENO' ERR
+# autodeploy.sh
+# Run at 20:00 everyday,update source code from svn.and rebuild *.war
+# Push *war to remote server,default test_server
+# Send mail to ${ERROR_REPORT} when Script execution failed
+# Modified by shidg,20150610
 
 # Define Variables
 source ~/.bashrc
 SOURCEDIR=/Data/source/trunk/
 BUILDDIR=/Data/war/trunk
+DEMOBUILDDIR=/Data/war/v0.6
 SVN_SERVER=svn.feezu.cn
 REPOSITORY_NAME="repos/wzc/manage/source/trunk"
 SVN_USER=jira
-SVN_PASS=jira
+SVN_PASS=jira*12345
 SYNC_USER=rsync_user
 TEST=10.10.8.32
 DEV=10.10.8.31
+DEMO=123.56.86.141
 FINAL_WEB=10.10.8.34
 FINAL_SERVICE=10.10.8.35
 REMOTE_SERVER=$TEST
 MODULE=webapps
-MAIL_LIST=94586572@qq.com,op@feezu.cn,ceshi@feezu.cn,jishu@feezu.cn
+ARG1=$1
 #MAIL_LIST=94586572@qq.com
+ERROR_REPORT=94586572@qq.com,332819226@qq.com,luokui@feezu.cn,songting@feezu.cn,wangliang@feezu.cn,maocc@feezu.cn,liuzhen@feezu.cn
+#ERROR_REPORT=94586572@qq.com
 
+
+#Trap Err Function
+function ERRTRAP(){
+    if [ ! -z "$ARG1" ];then
+        mail -s "${ARG1}_server redeploy failed" ${ERROR_REPORT} < /tmp/audpfail.log
+    else
+        mail -s "test_server redeploy failed" ${ERROR_REPORT} < /tmp/audpfail.log
+    fi
+    exit
+}
+
+trap 'ERRTRAP $LINENO' ERR
 
 #activemq server addr
 if [ "$1" = "final" ];then
@@ -38,52 +46,65 @@ if [ "$1" = "final" ];then
     DIR=(manage-datawarehouse/ manage-metadata/ manage-orders/)
     POSITION=src/main/resources/
     FILENAME=msgConfig.properties
-    for D in ${DIR[*]}
-        do  
-            sed -i "s/127.0.0.1/${FINAL_SERVICE}/" $SOURCEDIR/$D$POSITION$FILENAME
-        done
+        for D in ${DIR[*]}
+            do  
+                sed -i "s/127.0.0.1/${FINAL_SERVICE}/" $SOURCEDIR/$D$POSITION$FILENAME
+            done
 elif [ "$1" = "dev" ];then
     REMOTE_SERVER=$DEV
+elif [ "$1" = "demo" ];then
+    REMOTE_SERVER=$DEMO
+    sed -i 's/111.202.44.157/demo.feezu.cn/' ${SOURCEDIR}manage-orders/src/main/resources/acp_sdk.properties
+    sed -i 's/hbase.feezu.cn/10.162.196.41/' ${SOURCEDIR}manage-datawarehouse/src/main/resources/hbase-site.xml 
+    sed -i 's/image.feezu.cn/demo.feezu.cn/' ${SOURCEDIR}manage-metadata/src/main/resources/ftpconfig.properties
+    sed -i 's/image.feezu.cn/demo.feezu.cn/' ${SOURCEDIR}manage-web/src/main/resources/ftpconfig.properties
 fi
-
+                                                
 # begin
 PROJS2=(manage-web consumer-app manage-metadata manage-datawarehouse manage-report manage-orders)
 PROJCPMTEXT=(metadata report orders analysis manage app)
-
-
-if [ "`ls -A ${BUILDDIR}`" = " " ];then
-    svn co svn://${SVN_SERVER}/${REPOSITORY_NAME} ${BUILDDIR}/ --username ${SVN_USER} --password ${SVN_PASS}
-fi
-
-START_TIME=`date "+%Y%m%d-%T"`
+    
+    
+    if [ "`ls -A ${SOURCEDIR}`" = " " ];then
+        svn co https://${SVN_SERVER}/${REPOSITORY_NAME} ${SOURCEDIR}/ --username ${SVN_USER} --password ${SVN_PASS}
+        fi
+        
+        START_TIME=`date "+%Y%m%d-%T"`
 # update source code
-svn up ${SOURCEDIR}
-svn info ${SOURCEDIR} | tee /tmp/svninfo
-sed -i '=' /tmp/svninfo && sed -i 'N;s/\n/-/' /tmp/svninfo
-VERSION=`sed -n '/^[369]/p' /tmp/svninfo | awk -F '-' '{print $2}'`
+        svn up ${SOURCEDIR}
+        svn info ${SOURCEDIR} | tee /tmp/svninfo
+        sed -i '=' /tmp/svninfo && sed -i 'N;s/\n/-/' /tmp/svninfo
+        VERSION=`sed -n '/^[369]/p' /tmp/svninfo | awk -F '-' '{print $2}'`
 #build war packages
-cd ${SOURCEDIR}/wzc
-mvn clean package
-
+        cd ${SOURCEDIR}/wzc
+        mvn clean package
+        
 # delete old wars
-rm -rf ${BUILDDIR}/*
-
-# move war to /Data/war/trunk
-for PROJ in ${PROJS2[*]}
-    do
-	    mv ${SOURCEDIR}/${PROJ}/target/*war ${BUILDDIR}
-    done
-
+#        rm -rf ${BUILDDIR}/*
+                             
+# delete old wars & move war to /Data/war/trunk or v0.6
+if [ "$1" = "demo" ];
+then
+    rm -rf ${DEMOBUILDDIR}/*
+    for PROJ in ${PROJS2[*]}
+        do
+            mv ${SOURCEDIR}/${PROJ}/target/*war ${DEMOBUILDDIR}
+        done
+else
+    rm -rf ${BUILDDIR}/*
+    for PROJ in ${PROJS2[*]}
+        do
+            mv ${SOURCEDIR}/${PROJ}/target/*war ${BUILDDIR}
+        done
+fi                
 # rsync war to remote server
 if [ "$1" = "final" ];then
-    rsync -az --delete --exclude="app-download" --password-file=/etc/rsync.pass ${BUILDDIR}/{app.war,manage.war} ${SYNC_USER}@${REMOTE_SERVER[0]}::$MODULE
-    rsync -az --delete --exclude="app-download" --password-file=/etc/rsync.pass ${BUILDDIR}/{analysis.war,metadata.war,orders.war,report.war} ${SYNC_USER}@${REMOTE_SERVER[1]}::$MODULE
-
+    rsync -az --password-file=/etc/rsync.pass ${BUILDDIR}/{app.war,manage.war} ${SYNC_USER}@${REMOTE_SERVER[0]}::$MODULE
+    rsync -az --password-file=/etc/rsync.pass ${BUILDDIR}/{analysis.war,metadata.war,orders.war,report.war} ${SYNC_USER}@${REMOTE_SERVER[1]}::$MODULE
+        
 #restart tomcat on ${REMOTE_SERVER}
-(ssh F_MANAGE "/bin/bash /Data/scripts/restart_tomcat.sh")                                                     
-wait 
-(ssh F_SERVICE "/bin/bash /Data/scripts/restart_tomcat.sh")
-wait
+ssh F_MANAGE "/Data/scripts/restart_tomcat.sh"                                                     
+ssh F_SERVICE "/Data/scripts/restart_tomcat.sh"
 
 END_TIME=`date "+%Y%m%d-%T"`
 
@@ -97,7 +118,7 @@ $VERSION
 EOF
 
 #NOTICE
-mail -s "redeploy success" ${MAIL_LIST} < /tmp/upinfo
+#mail -s "redeploy success" ${MAIL_LIST} < /tmp/upinfo
 
 #log
 cat /tmp/upinfo >> /Data/logs/wzc/autodeploy.log
@@ -107,9 +128,12 @@ for D in ${DIR[*]}
     do  
         sed -i "s/${FINAL_SERVICE}/127.0.0.1/" $SOURCEDIR/$D$POSITION$FILENAME
     done
-
+                
 elif [ "$1" = "dev" ];then
-    rsync -az --delete --exclude="app-download" --password-file=/etc/rsync.pass ${BUILDDIR}/ ${SYNC_USER}@${REMOTE_SERVER}::$MODULE
+    rsync -az --delete --exclude="app-download" --exclude="ROOT" --exclude="host-manager" --exclude="manager" --password-file=/etc/rsync.pass ${BUILDDIR}/ ${SYNC_USER}@${REMOTE_SERVER}::$MODULE
+
+#解压cacti监控tomcat所需目录    
+#ssh  -n DEV  "tar -zxvf /Data/app/tomcat/webapps/ctineed.tar.gz -C /Data/app/tomcat/webapps/"
 #restart tomcat on $(REMOTE_SERVER)
 ssh  -n DEV  "/Data/scripts/restart_tomcat.sh"
 
@@ -125,33 +149,61 @@ $VERSION
 EOF
 
 #notice
-mail -s "redeploy success" ${MAIL_LIST} < /tmp/upinfo
+#mail -s "redeploy success" ${MAIL_LIST}< /tmp/upinfo
 
 #log
 cat /tmp/upinfo >> /Data/logs/wzc/autodeploy.log
 
-else    
-    rsync -az --delete --exclude="app-download" --password-file=/etc/rsync.pass ${BUILDDIR}/ ${SYNC_USER}@${REMOTE_SERVER}::$MODULE
+elif [ "$1" = "demo" ];then
+    rsync -az --delete --exclude="app-down" --exclude="download" --exclude="ROOT" --exclude="host-manager" --exclude="manager" --password-file=/etc/rsync.pass ${DEMOBUILDDIR}/ ${SYNC_USER}@${REMOTE_SERVER}::$MODULE
+
+#解压cacti监控tomcat所需目录    
+#    ssh TEST "tar -zxvf /Data/app/tomcat/webapps/ctineed.tar.gz -C /Data/app/tomcat/webapps/"
 #restart tomcat on $(REMOTE_SERVER)
-    ssh TEST "/Data/scripts/restart_tomcat.sh" 
+    ssh TEST_ONLINE "/Data/scripts/restart_tomcat.sh"
 
 END_TIME=`date "+%Y%m%d-%T"`
-
-cat > /tmp/upinfo <<EOF
-=========================
+ 
+      cat > /tmp/upinfo <<EOF
+     =========================
+server:demo.feezu.cn
+Start at:${START_TIME}
+Finish at:${END_TIME}
+Description:
+$VERSION
+EOF
+#notice
+#    mail -s "redeploy success" ${MAIL_LIST} < /tmp/upinfo
+#log
+    cat /tmp/upinfo >> /Data/logs/wzc/autodeploy.log
+##银联回调地址恢复
+    sed -i 's/demo.feezu.cn/111.202.44.157/' ${SOURCEDIR}manage-orders/src/main/resources/acp_sdk.properties
+    sed -i 's/10.162.196.41/hbase.feezu.cn/' ${SOURCEDIR}manage-datawarehouse/src/main/resources/hbase-site.xml 
+    sed -i 's/demo.feezu.cn/image.feezu.cn/' ${SOURCEDIR}manage-metadata/src/main/resources/ftpconfig.properties
+    sed -i 's/demo.feezu.cn/image.feezu.cn/' ${SOURCEDIR}manage-web/src/main/resources/ftpconfig.properties
+else    
+    rsync -az --delete --exclude="app-download"  --exclude="ROOT" --exclude="host-manager" --exclude="manager" --password-file=/etc/rsync.pass ${BUILDDIR}/ ${SYNC_USER}@${REMOTE_SERVER}::$MODULE
+#restart tomcat on $(REMOTE_SERVER)
+    ssh TEST "/Data/scripts/restart_tomcat.sh" 
+#解压cacti监控tomcat所需目录    
+# ssh TEST "tar -zxvf /Data/app/tomcat/webapps/ctineed.tar.gz -C /Data/app/tomcat/webapps/"
+    
+    END_TIME=`date "+%Y%m%d-%T"`
+    
+    cat > /tmp/upinfo <<EOF
+    =========================
 server:test_server
 Start at:${START_TIME}
 Finish at:${END_TIME}
 Description:
 $VERSION
 EOF
-
+        
 #notice
-mail -s "redeploy success" ${MAIL_LIST} < /tmp/upinfo
-
+#        mail -s "redeploy success" ${MAIL_LIST} < /tmp/upinfo
 #log
-cat /tmp/upinfo >> /Data/logs/wzc/autodeploy.log
+        cat /tmp/upinfo >> /Data/logs/wzc/autodeploy.log
 fi
-
+         
 # Done
 exit 0
