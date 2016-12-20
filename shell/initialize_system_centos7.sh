7eb8a2e6c656ab324f8d636cee77e5dea198f36e
shidg <shjiayeah@gmail.com>
2016-12-20 11:52:50 +0800

    modified:   shell/initialize_system_centos7.sh

--- shell/initialize_system_centos7.sh
+++ shell/initialize_system_centos7.sh
@@ -3,6 +3,7 @@
 if [ $(id -u) != 0 ];then
 echo "Must be root can do this."
 exit 9
+fi
 
 #selinux
 if [ `sed -n '/^SELINUX=/p' /etc/selinux/config  | cut -d= -f2` != disabled ];then
@@ -19,7 +20,6 @@ yum install iptables-services -y
 port=5122
 sed  -i "/^#Port/ {s/^#//;s/22/$port/}" /etc/ssh/sshd_config
 
-fi
 # set privileges
 chmod 600 /etc/shadow
 chmod 600 /etc/gshadow
@@ -303,53 +303,6 @@ inoremap ( ()<LEFT>
 inoremap [ []<LEFT>
 inoremap { {}<LEFT>
 
-" 按退格键时判断当前光标前一个字符，如果是左括号，则删除对应的右括号以及括号中间的内容
-function! RemovePairs()
-    let l:line = getline(".")
-    let l:previous_char = l:line[col(".")-1] " 取得当前光标前一个字符
- 
-    if index(["(", "[", "{"], l:previous_char) != -1
-        let l:original_pos = getpos(".")
-        execute "normal %"
-        let l:new_pos = getpos(".")
- 
-        " 如果没有匹配的右括号
-        if l:original_pos == l:new_pos
-            execute "normal! a\<BS>"
-            return
-        end
- 
-        let l:line2 = getline(".")
-        if len(l:line2) == col(".")
-            " 如果右括号是当前行最后一个字符
-            execute "normal! v%xa"
-        else
-            " 如果右括号不是当前行最后一个字符
-            execute "normal! v%xi"
-        end
- 
-    else
-        execute "normal! a\<BS>"
-    end
-endfunction
-
-" 用退格键删除一个左括号时同时删除对应的右括号
-inoremap <BS> <ESC>:call RemovePairs()<CR>a
-
-" 输入一个字符时，如果下一个字符也是括号，则删除它，避免出现重复字符
-function! RemoveNextDoubleChar(char)
-    let l:line = getline(".")
-    let l:next_char = l:line[col(".")] " 取得当前光标后一个字符
- 
-    if a:char == l:next_char
-        execute "normal! l"
-    else
-        execute "normal! i" . a:char . ""
-    end
-endfunction
-inoremap ) <ESC>:call RemoveNextDoubleChar(')')<CR>a
-inoremap ] <ESC>:call RemoveNextDoubleChar(']')<CR>a
-inoremap } <ESC>:call RemoveNextDoubleChar('}')<CR>a
  
 "在执行宏命令时，不进行显示重绘；在宏命令执行完成后，一次性重绘，以便提高性能。
 set lazyredraw
@@ -360,16 +313,12 @@ set tabstop=4
 "在按退格键时，如果前面有4个空格，则会统一清除
 set softtabstop=4
  
-"cindent对c语法的缩进更加智能灵活，
-"而shiftwidth则是在使用&lt;和&gt;进行缩进调整时用来控制缩进量。
-"换行自动缩进，是按照shiftwidth值来缩进的
-set cindent shiftwidth=4
  
 "最基本的自动缩进
 "set autoindent shiftwidth=4
  
 "比autoindent稍智能的自动缩进
-set smartindent shiftwidth=4
+"set smartindent shiftwidth=4
 
 "字符编码
 set encoding=utf-8
