4ad826433c65186893807519068b2924e97fb6ba
史德刚 <shidg@feezu.cn>
2018-10-17 10:49:55 +0800

    new file:   nexus/install.txt 	modified:   shell/initialize_system_centos7.sh 	new file:   shell/show_ths.sh 	modified:   zabbix/install.sh

--- shell/initialize_system_centos7.sh
+++ shell/initialize_system_centos7.sh
@@ -121,14 +121,15 @@ net.ipv4.tcp_max_syn_backlog = 8192
 net.ipv4.tcp_synack_retries = 2
 net.ipv4.tcp_syn_retries = 2
 
+# tcp时间戳
+net.ipv4.tcp_timestamps = 1
+
 # TIME-WAIT快速回收,须开启tcp时间戳方能生效
 net.ipv4.tcp_tw_recycle = 1
 
 # TIME_WAIT重用,须开启tcp时间戳方能生效,服务器前端存在NAT设备的时候尽量关闭快速回收
 net.ipv4.tcp_tw_reuse = 1
 
-# tcp时间戳
-net.ipv4.tcp_timestamps = 1
 
 #系统同时保持TIME_WAIT套接字的最大数量
 net.ipv4.tcp_max_tw_buckets = 5000
--- zabbix/install.sh
+++ zabbix/install.sh
@@ -21,9 +21,9 @@
 yum install net-snmp-devel libxml2-devel curl curl-devel unixODBC unixODBC-devel
 
 #install
-tar zxvf zabbix-2.4.5.tar.gz
-cd zabbix-2.4.5
-./configure --prefix=/Data/app/zabiix-2.4.5 --enable-server --enable-proxy --enable-java --enable-agent --with-mysql=/Data/app/mysql/bin/mysql_config --with-net-snmp --with-libiconv=/usr/ --with-libcurl --with-unixodbc --with-ldap
+tar zxvf zabbix-4.0.0.tar.gz
+cd zabbix-4.0.0
+./configure --prefix=/Data/app/zabbix-4.0.0 --enable-server --enable-proxy --enable-java --enable-agent --with-mysql=/Data/app/mysql/bin/mysql_config --with-net-snmp --with-libiconv=/usr/ --with-libcurl --with-unixodbc --with-ldap
 make && make install
 
 #MySQL
@@ -39,7 +39,7 @@ flush privileges;
 EOF
 
 mkdir /etc/zabbix
-cd /Data/software/zabbix-2.4.5
+cd /Data/software/zabbix-4.0.0
 cp conf/zabbix_server.conf /etc/zabbix
 #zabbix_server.conf基础配置
 #DBName=localhost
@@ -54,17 +54,63 @@ useradd zabbix
 /Data/app/zabbix/sbin/zabbix_java/startup.sh
 
 #nginx配置好虚拟机，如 vhosts/monitor.my.com，假设根目录设置为/Data/code/zabbix
-cd /Data/software/zabbix-2.4.5
+cd /Data/software/zabbix-4.0.0
 cp -a frontends/php/* /Data/code/zabbix
 #http://monitor.my.com开始zabbix的配置及访问,初始用户名密码admin/zabbix
 
 # zabbix client
-tar zxvf zabbix-3.2.3.tar.gz && cd zabbix-3.2.3
-./configure --prefix=/Data/app/zabbix-3.2.3/ --enable-agent
+tar zxvf zabbix-4.0.0.tar.gz && cd zabbix-4.0.0
+./configure --prefix=/Data/app/zabbix-4.0.0/ --enable-agent
 make && make install
 
 mkdir /etc/zabbix
 cp /etc/zabbix_agentd.conf /etc/zabbix/
 
 useradd zabbix
-/Data/app/zabbix-3.2.3/sbin/zabbix_agentd -c /etc/zabbix/zabbix_agentd.conf
+/Data/app/zabbix-4.0.0/sbin/zabbix_agentd -c /etc/zabbix/zabbix_agentd.conf
+
+
+# 邮件报警配置（脚本方式）
+
+# 添加媒介类型。 管理-->报警媒介类型-->创建媒体类型
+# 名称 SendEmail,类型 脚本 ,脚本名称 SendEmail.sh, 脚本参数共三个: {ALERT.SENDTO} {ALERT.SUBJECT} {ALERT.MESSAGE}
+
+
+
+# 添加报警动作。 配置-->动作-->创建动作
+
+#名称 CRITICAL 条件 自定义
+
+#操作：
+#默认标题: 
+[Zabbix] 故障通知,服务器:{HOSTNAME1}发生: {TRIGGER.NAME}故障!
+
+#消息内容：
+故障主机: {HOST.NAME1}<br />
+故障时间: {EVENT.DATE} {EVENT.TIME}<br />
+故障等级: {TRIGGER.SEVERITY}<br />
+告警信息: {TRIGGER.NAME}<br />
+问题详情: {ITEM.NAME}:{ITEM.VALUE}<br />
+当前状态: {TRIGGER.STATUS}<br />
+事件ID: {EVENT.ID}
+
+#发送细节自定义
+
+
+# 恢复操作
+#默认标题
+[Zabbix] 恢复通知. 服务器:{HOSTNAME1}已恢复!
+
+# 消息内容
+恢复主机:{HOSTNAME1} <br />
+恢复时间:{EVENT.RECOVERY.DATE} {EVENT.RECOVERY.TIME}<br />
+告警等级:{TRIGGER.SEVERITY}<br />
+告警信息: {TRIGGER.NAME}<br />
+当前状态:{TRIGGER.STATUS} ({ITEM.VALUE1})<br />
+事件ID:{EVENT.ID}
+
+#发送细节
+Notify all involved	
+
+# 添加用户，为用户配置媒介类型、接收邮箱等信息
+# 邮件报警配置完成
