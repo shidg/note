9a5a1f5124fecd1e0c4a40282748e119075d3972
shidg <shjiayeah@gmail.com>
2019-12-27 12:10:09 +0800

    modify sysctl.conf

---
+++ python/install_python_on_centos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# File Name: -- install_python_on_centos.sh --
+# author: -- shidegang --
+# Created Time: 2019-12-26 10:14:11
+
+
+## python 3.x ###
+yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel gcc libffi-devel gcc make automake autoconf libtool libffi-devel
+
+# https://www.python.org/downloads/
+
+./configure --prefix=/usr/local/python3 --enable-optimizations
+make && make install
+
+
