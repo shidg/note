ed25da385ed38e334935430e85a6de4eacb2d0d6
hanker <shjiayeah@gmail.com>
2020-01-05 10:39:06 +0800

    新文件：   cert-manager/cert-manager.yaml 	新文件：   cert-manager/ingress_example.yaml 	新文件：   cert-manager/install.sh 	新文件：   cert-manager/letsencrypt-prod.yaml

---
+++ k8s/cert-manager/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# File Name: -- install.sh --
+# author: -- shidegang --
+# Created Time: 2020-01-04 22:49:06
+
+## cert-manager v0.12.0
+
+# install cert-manager
+
+kubectl create namespace cert-manager
+
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v0.12.0/cert-manager.yaml
+# 在cert-manager.yaml的(Deployment)cert-manager.spec.template.spec.container.args下添加了三行内容作为启动参数
+- --default-issuer-name=letsencrypt-prod
+- --default-issuer-kind=ClusterIssuer
+- --default-issuer-group=cert-manager.io
+三行内容的作用是支持ingress中使用"kubernetes.io/tls-acme: "true""
+
+# ClusterIssuer 用来从Let’s Encrypt申请证书
+kubectl apply -f letsencrypt-prod.yaml
+
+# LoadBalancer类型的nginx-ingress,并在ingress 中添加以下注解:
+#kubernetes.io/tls-acme: "true"
+or
+#cert-manager.io/cluster-issuer: letsencrypt-prod
+
+# 在开始为域名申请证书之前，应该保证该域名已经解析到了nginx-ingress的外网地址（ExternalIP）
+
