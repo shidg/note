770671cda9f7818167f288cac0e7703c8720da30
hanker <shjiayeah@gmail.com>
2023-02-01 16:16:18 +0800

    修改：     k8s/nfs_storageclass/howto.txt 	修改：     k8s/v1.22/one_master_install.sh

--- k8s/v1.22/one_master_install.sh
+++ k8s/v1.22/one_master_install.sh
@@ -137,7 +137,7 @@ baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
-gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg
 EOF
 
 # 更新缓存
