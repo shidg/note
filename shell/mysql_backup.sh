6d32e6ab6870c642a000da2b6183af645bc0f03f
shidg <shjiayeah@gmail.com>
2025-08-13 16:18:21 +0800

    modified:   mysql_backup.sh 	new file:   readme.md 	deleted:    readme.txt

--- shell/mysql_backup.sh
+++ shell/mysql_backup.sh
@@ -7,7 +7,7 @@ BACK_DIR=/Data/backup/mysql
 
 DB_LST=/tmp/db.lst
 
-DATE=`date +%Y-%m-%d` 
+DATE=`date +%Y-%m-%d`
 
 export PATH=$PATH:/Data/app/mysql/bin
 
@@ -16,8 +16,8 @@ mysql -uroot -p9bdgvYHkVRTM9 -e 'show databases' > $DB_LST
 [ ! -d $BACK_DIR ] && mkdir -p $BACK_DIR
 for i in $(grep -vE "Database|information_schema|performance_schema|mysql" $DB_LST)
 do
-	mysqldump -uroot -p9bdgvYHkVRTM9 --default-character-set=utf8 --single-transactioin --skip-lock-tables --quick $i > $BACK_DIR/$i-$DATE.sql
-	[ "$PWD" != "$BACK_DIR" ] && cd $BACK_DIR 
+	mysqldump -uroot -p9bdgvYHkVRTM9 --default-character-set=utf8 --single-transaction --skip-lock-tables --quick $i > $BACK_DIR/$i-$DATE.sql
+	[ "$PWD" != "$BACK_DIR" ] && cd $BACK_DIR
 	gzip  -f $i-$DATE.sql
 done
 
